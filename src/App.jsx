b4da75794608d2b2bb94fd332bda0f5b3c493b2e
Hamidjonov-Asrorbek <asrorbekhamidjonov1@gmail.com>
2024-06-12 09:50:07 +0500

    .

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,42 @@
 import React from "react";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import {
+  createBrowserRouter,
+  Navigate,
+  RouterProvider,
+} from "react-router-dom";
 import Layout from "./Layout";
 import Products from "./pages/Products";
+import Login from "./components/Login";
+import SignUp from "./components/SignUp";
 
 function App() {
+  const ProtectedRoute = ({ children }) => {
+    const user = JSON.parse(localStorage.getItem("user")) ?? false;
+
+    return user ? children : <Navigate to={"/login"} />;
+  };
   const router = createBrowserRouter([
     {
       path: "/",
-      element: <Layout />,
+      element: (
+        <ProtectedRoute>
+          <Layout />
+        </ProtectedRoute>
+      ),
       children: [
         {
           path: "/",
           element: <Products />,
         },
       ],
+    },
+    {
+      path: "/login",
+      element: <Login />,
+    },
+    {
+      path: "/signup",
+      element: <SignUp />,
     },
   ]);
 
