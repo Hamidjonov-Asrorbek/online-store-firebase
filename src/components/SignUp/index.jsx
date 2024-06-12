b16cb2586f88b7abca7e03fe2275501acc8a9d9d
Hamidjonov-Asrorbek <asrorbekhamidjonov1@gmail.com>
2024-06-12 13:50:22 +0500

    .

--- src/components/SignUp/index.jsx
+++ src/components/SignUp/index.jsx
@@ -1,16 +1,22 @@
+import { createUserWithEmailAndPassword, updateProfile } from "firebase/auth";
 import React, { useState } from "react";
+import { auth } from "../../firebase/config";
+import { useNavigate } from "react-router-dom";
 
 function SignUp() {
-  const [SignData, setSignData] = useState({
+  const navigate = useNavigate();
+  const [signData, setSignData] = useState({
     email: "",
     password: "",
+    displayName: "",
+    photoURL: "",
   });
   const [errors, setErrors] = useState({
     email: "",
     password: "",
     firebase: "",
   });
-
+  const [register, setRegister] = useState(false);
   const validateEmail = (email) => {
     const re = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     return re.test(String(email).toLowerCase());
@@ -42,59 +48,147 @@ function SignUp() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    console.log(SignData);
+    console.log(signData);
+    createUserWithEmailAndPassword(auth, signData.email, signData.password)
+      .then((userCredential) => {
+        // Signed in
+        const user = userCredential.user;
+        console.log(user);
+        setRegister(true);
+      })
+      .catch((error) => {
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        console.log(errorCode, errorMessage);
+        setErrors((prevErrors) => ({
+          ...prevErrors,
+          firebase: "Failed to sign up. Please check your credentials.",
+        }));
+      });
+  };
+  const handleUpdateProfile = (e) => {
+    e.preventDefault();
+    updateProfile(auth.currentUser, {
+      displayName: signData.email.split("@")[0],
+      photoURL: signData.email,
+    })
+      .then(() => {
+        console.log("Profile updated");
+        navigate("/");
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+    setRegister(false);
   };
   return (
-    <form className="w-1/3 flex flex-col gap-5 m-auto" onSubmit={handleSubmit}>
-      <h1 className="text-3xl text-center">Sign Up</h1>
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
+    <>
+      {!register ? (
+        <form
+          className="w-1/3 flex flex-col gap-5 m-auto"
+          onSubmit={handleSubmit}
         >
-          <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
-          <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
-        </svg>
-        <input
-          type="email"
-          className="grow"
-          placeholder="Email"
-          value={SignData.email}
-          onChange={(e) => setSignData({ ...SignData, email: e.target.value })}
-        />
-      </label>
-      {errors.email && <p className="text-red-500">{errors.email}</p>}
-      <label className="input input-bordered flex items-center gap-2">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          viewBox="0 0 16 16"
-          fill="currentColor"
-          className="w-4 h-4 opacity-70"
+          <h1 className="text-3xl text-center">Sign Up</h1>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path d="M2.5 3A1.5 1.5 0 0 0 1 4.5v.793c.026.009.051.02.076.032L7.674 8.51c.206.1.446.1.652 0l6.598-3.185A.755.755 0 0 1 15 5.293V4.5A1.5 1.5 0 0 0 13.5 3h-11Z" />
+              <path d="M15 6.954 8.978 9.86a2.25 2.25 0 0 1-1.956 0L1 6.954V11.5A1.5 1.5 0 0 0 2.5 13h11a1.5 1.5 0 0 0 1.5-1.5V6.954Z" />
+            </svg>
+            <input
+              type="email"
+              className="grow"
+              placeholder="Email"
+              value={signData.email}
+              onChange={(e) =>
+                setSignData({ ...signData, email: e.target.value })
+              }
+            />
+          </label>
+          {errors.email && <p className="text-red-500">{errors.email}</p>}
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path
+                fillRule="evenodd"
+                d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
+                clipRule="evenodd"
+              />
+            </svg>
+            <input
+              type="password"
+              className="grow"
+              placeholder="Password"
+              value={signData.password}
+              onChange={(e) =>
+                setSignData({ ...signData, password: e.target.value })
+              }
+            />
+          </label>
+          {errors.password && <p className="text-red-500">{errors.password}</p>}
+          {errors.firebase && <p className="text-red-500">{errors.firebase}</p>}
+          <button type="submit" className="btn btn-primary">
+            Sign Up
+          </button>
+        </form>
+      ) : (
+        <form
+          className="w-1/3 flex flex-col gap-5 m-auto"
+          onSubmit={handleUpdateProfile}
         >
-          <path
-            fillRule="evenodd"
-            d="M14 6a4 4 0 0 1-4.899 3.899l-1.955 1.955a.5.5 0 0 1-.353.146H5v1.5a.5.5 0 0 1-.5.5h-2a.5.5 0 0 1-.5-.5v-2.293a.5.5 0 0 1 .146-.353l3.955-3.955A4 4 0 1 1 14 6Zm-4-2a.75.75 0 0 0 0 1.5.5.5 0 0 1 .5.5.75.75 0 0 0 1.5 0 2 2 0 0 0-2-2Z"
-            clipRule="evenodd"
-          />
-        </svg>
-        <input
-          type="password"
-          className="grow"
-          placeholder="Password"
-          value={SignData.password}
-          onChange={(e) =>
-            setSignData({ ...SignData, password: e.target.value })
-          }
-        />
-      </label>
-      {errors.password && <p className="text-red-500">{errors.password}</p>}
-      {errors.firebase && <p className="text-red-500">{errors.firebase}</p>}
-      <button type="submit" className="btn btn-primary">
-        Sign Up
-      </button>
-    </form>
+          <h1 className="text-3xl text-center">User info</h1>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 16 16"
+              fill="currentColor"
+              className="w-4 h-4 opacity-70"
+            >
+              <path d="M8 8a3 3 0 1 0 0-6 3 3 0 0 0 0 6ZM12.735 14c.618 0 1.093-.561.872-1.139a6.002 6.002 0 0 0-11.215 0c-.22.578.254 1.139.872 1.139h9.47Z" />
+            </svg>
+            <input
+              type="text"
+              className="grow"
+              placeholder="Username"
+              value={signData.displayName}
+              onChange={(e) =>
+                setSignData({ ...signData, displayName: e.target.value })
+              }
+            />
+          </label>
+          <label className="input input-bordered flex items-center gap-2">
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="w-6 h-6 opacity-70"
+            >
+              <path d="M12 2a1 1 0 0 1 1 1v5h2.586l-3.293-3.293a1 1 0 1 1 1.414-1.414L16.707 7.586A1 1 0 0 1 16 9H13v4a1 1 0 0 1-2 0V9H8a1 1 0 0 1-.707-1.707L10.293 4.707a1 1 0 0 1 1.414 1.414L8.414 10H11V3a1 1 0 0 1 1-1zM5 14a3 3 0 1 0 0 6 3 3 0 0 0 0-6zm14-1a1 1 0 0 0-1 1v4.172l-1.586-1.586a2 2 0 0 0-2.828 0L11.828 20.172a1 1 0 0 0 0 1.414L16.586 27a2 2 0 0 0 2.828 0L22 24.414V18a1 1 0 0 0-1-1h-2zm1 6.586L19.414 21H21v2.586z" />
+            </svg>
+            <input
+              type="link"
+              className="grow"
+              placeholder="Image link"
+              value={signData.photoURL}
+              onChange={(e) =>
+                setSignData({ ...signData, photoURL: e.target.value })
+              }
+            />
+          </label>
+          <button type="submit" className="btn btn-primary">
+            User create
+          </button>
+        </form>
+      )}
+    </>
   );
 }
 
